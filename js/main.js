128ddb95ac16152376652a1d4081ef0f4432a480
Saaayurii <relmontovro@gmail.com>
2023-05-05 19:07:43 +0300

    six

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(function () {
- 
   $(".rateYo").rateYo({
     starWidth: "40px"
   });
+  
  
+  var mixer = mixitup('.Blog__list');
 });
 $('.card-deck a').fancybox({
   caption : function( instance, item ) {
