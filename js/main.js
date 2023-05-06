7e4aada38128b8ca3180a9f54d5bd61a7b547e50
Saaayurii <relmontovro@gmail.com>
2023-05-07 01:51:01 +0300

    New slider + new slide

--- js/main.js
+++ js/main.js
@@ -5,9 +5,27 @@ $(function () {
   
  
   var mixer = mixitup('.Blog__list');
-});
-$('.card-deck a').fancybox({
-  caption : function( instance, item ) {
-    return $(this).parent().find('.card-text').html();
-  }
+  $('.card-deck a').fancybox({
+    caption : function( instance, item ) {
+      return $(this).parent().find('.card-text').html();
+    }
+  });
+
+  $('.Customers__slider').slick({
+    arrows: false,
+    slidesToShow:2,
+    infinite:true,
+    draggable: false,
+    waitForAnimate: false,
+  })
+  $('.Customers__slider-prev').on('click', function(e){
+    e.preventDefault()
+    $('.Customers__slider').slick('slickPrev')
+  })
+  $('.Customers__slider-next').on('click', function(e){
+    e.preventDefault()
+    $('.Customers__slider').slick('slickNext')
+  })
+  
+  
 });
