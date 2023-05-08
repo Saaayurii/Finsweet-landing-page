5939388a1c4989978368ba8fdfc9af35a0543fa6
Saaayurii <relmontovro@gmail.com>
2023-05-09 01:56:25 +0300

    plus and minus

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
 $(function () {
   $(".rateYo").rateYo({
-    starWidth: "40px"
-  });
-  
- 
-  var mixer = mixitup('.Blog__list');
-  $('.card-deck a').fancybox({
-    caption : function( instance, item ) {
-      return $(this).parent().find('.card-text').html();
-    }
+    starWidth: "40px",
   });
 
-  $('.Customers__slider').slick({
+  var mixer = mixitup(".Blog__list");
+  $(".card-deck a").fancybox({
+    caption: function (instance, item) {
+      return $(this).parent().find(".card-text").html();
+    },
+  });
+
+  $(".Customers__slider").slick({
     arrows: false,
-    slidesToShow:2,
-    infinite:true,
+    slidesToShow: 2,
+    infinite: true,
     draggable: false,
     waitForAnimate: false,
     dots: true,
-    appendDots: $('.Customers__dots'),
-  })
-  $('.Customers__slider-prev').on('click', function(e){
-    e.preventDefault()
-    $('.Customers__slider').slick('slickPrev')
-  })
-  $('.Customers__slider-next').on('click', function(e){
-    e.preventDefault()
-    $('.Customers__slider').slick('slickNext')
-  })
-  
-  $(".Questions__acc-link").on('click', function(e){
-e.preventDefault()
-$(this).toggleClass('Questions__acc-link--active')
-$(this).children('.Questions__acc-text').slideToggle()
+    appendDots: $(".Customers__dots"),
+  });
+  $(".Customers__slider-prev").on("click", function (e) {
+    e.preventDefault();
+    $(".Customers__slider").slick("slickPrev");
+  });
+  $(".Customers__slider-next").on("click", function (e) {
+    e.preventDefault();
+    $(".Customers__slider").slick("slickNext");
+  });
 
-  })
+  $(".Questions__acc-link").on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("Questions__acc-link--active");
+    $(this).children(".Questions__acc-text").slideToggle();
+  });
 });
