54249e1592ac1cb23033c2204356e428ec322217
Saaayurii <relmontovro@gmail.com>
2023-05-16 06:28:06 +0300

    adaptation

--- js/main.js
+++ js/main.js
@@ -39,8 +39,19 @@ $(function () {
       top = $(id).offset().top
     $("body,html").animate({ scrollTop: top }, 1000)
   })
-  $('.burger').on('click', function(e){
+  
+
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  }, 0)
+  $('.burger, .overlay, .header__top a').on('click', function (e) {
     e.preventDefault()
     $('.header__top').toggleClass('header__top--open')
+    $('.overlay').toggleClass('overlay--show')
   })
+
 });
